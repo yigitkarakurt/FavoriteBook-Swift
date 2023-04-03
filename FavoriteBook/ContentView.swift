f74d0ce5ed3c49b6aa7173ff9e458584ec43abfe
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-03 13:30:05 +0300

    navigation and details

--- FavoriteBook/ContentView.swift
+++ FavoriteBook/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            List {
+                ForEach(myFavorites) { favorite in
+                    Section(header: Text(favorite.title)){
+                        ForEach(favorite.elements) { element in
+                            NavigationLink(destination: DetailsView(chosenFavoriteElement: element)){
+                                HStack{
+                                    Image(systemName: "heart.fill")
+                                        .foregroundColor(.red)
+                                        
+                                    Text(element.name)
+                                }
+                                
+                            }
+                            
+                            
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
--- FavoriteBook/FavoriteModel.swift
+++ FavoriteBook/FavoriteModel.swift
@@ -23,8 +23,18 @@ struct FavoriteElements : Identifiable {
     var description : String
 }
 
+//Bands
 let eagles = FavoriteElements(name: "Eagles", imagename: "eagles", description: "No 1 Music Band")
 let megadeth = FavoriteElements(name: "Megadeth", imagename: "megadeth", description: "No 2 Music Band")
 let ironmaiden = FavoriteElements(name: "Iron Maiden", imagename: "ironmaiden", description: "No 3 Music Band")
 
 let favoriteBands = FavoriteModel(title: "Favorite Bands", elements: [eagles,megadeth,ironmaiden])
+
+//Movies
+let pulpFiction = FavoriteElements(name: "Pulp Fiction", imagename: "pulpfiction", description: "No 1 Movie")
+let thedarkknight = FavoriteElements(name: "The Dark Knight", imagename: "thedarkknigt", description: "No 2 Movie")
+let killbill = FavoriteElements(name: "Kill Bill", imagename: "killbill", description: "No 3 Movie")
+
+let favoriteMovies = FavoriteModel(title: "Favorite Movies", elements: [pulpFiction,thedarkknight,killbill])
+
+let myFavorites = [favoriteBands,favoriteMovies]
