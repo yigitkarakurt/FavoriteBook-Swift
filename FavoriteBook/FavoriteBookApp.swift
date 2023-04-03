4bdb190fd09cd2c6e2e945b419a62a5ce08babaf
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-03 11:30:33 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Yigit Karakurt on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
